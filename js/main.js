e4d3bd339e537f6f4b727bedf70e157e6f7071bc
Batzorig Tsergiinkhuu <batzorigtsergiinkhuu@Batzorigs-MBP.lan>
2017-10-16 10:46:26 +0800

    made most codes js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
-
 var titanic = new Titanic({hover:false});
-var checkbox = document.getElementById('titanic-checkbox');
+var animatedImages = ["cheap", "trash", "cloud"]
+var container = document.createElement('div')
+var row = document.createElement('div')
+container.className = "container"
+row.className = "row"
+for(var i = 0; i < 3; i++)
+{
+  var smallContainer = document.createElement('div')
+  smallContainer.className = "col roundedContainer text-center"
+  var h2 = document.createElement('h2')
+  h2.innerHTML = "Price";
+  var animatedImage = document.createElement('div')
+  animatedImage.className = "titanic titanic-"+ animatedImages[i] + " icon";
+  animatedImage.id = animatedImages[i]
+  smallContainer.append(h2)
+  smallContainer.append(animatedImage)
+  row.append(smallContainer)
+}
+container.append(row)
+document.body.append(container)
+
 $(".roundedContainer").on({
 
     mouseenter: function () {
